a9c2f19e06a627d595aa65800f72bbc8096af7c1
Josse2002 <75065069+Josse2002@users.noreply.github.com>
2021-08-10 21:30:54 -0600

    Create BuscarClientePorNombre.cs

---
+++ BuscarClientePorNombre.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CS.Facturacion.BL;
+using CS.Facturacion.Entities;
+using System.Data.SqlClient;
+
+namespace CS.Facturacion.UI.WF
+{
+    public partial class BuscarClientePorNombre : Form
+    {
+        public BuscarClientePorNombre()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarCliente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClienteBL _clienteBL = new ClienteBL();
+
+                if (txtBuscarCliente.Text.Trim() != "" )
+                {
+                    dgvBuscarCliente.DataSource = _clienteBL.BuscarClientePorNombres(txtBuscarCliente.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Debe Ingresar valores para realizar la busqueda ",
+                                        "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en la aplicacion: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (TimeoutException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en los campos: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en la aplicacion: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en la aplicacion: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtBuscarCliente_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+               
+                if (chkbBuscarNombre.Checked)
+                {
+                    ClienteBL _clienteBL = new ClienteBL();
+                    if (txtBuscarCliente.Text.Trim() != "")
+                    {
+                        dgvBuscarCliente.DataSource = _clienteBL.BuscarClientePorNombres(txtBuscarCliente.Text);
+                        
+                        
+                    }
+                    else
+                    {
+                        dgvBuscarCliente.DataSource = null;
+                    }
+
+                }
+                else if (chkbBuscarDUI.Checked)
+                {
+                    //CODIGO DUI
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Debe Ingresar valores para realizar la busqueda ",
+                                        "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+               
+
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en la aplicacion: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (TimeoutException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en los campos: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en la aplicacion: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error en la aplicacion: " + ex.Message,
+                    "Error en la aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void chkbBuscarNombre_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkbBuscarNombre.Checked)
+            {
+                chkbBuscarDUI.Enabled = false;
+
+                if (txtBuscarCliente.Enabled == false)
+                {
+                    txtBuscarCliente.Enabled = true;
+                }
+            }
+            else
+            {
+                chkbBuscarDUI.Enabled = true;
+                if (txtBuscarCliente.Enabled == true)
+                {
+                    txtBuscarCliente.Enabled = false;
+                }
+            }
+        }
+
+        private void chkbBuscarDUI_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkbBuscarDUI.Checked)
+            {
+                chkbBuscarNombre.Enabled = false;
+
+                if (txtBuscarCliente.Enabled == false)
+                {
+                    txtBuscarCliente.Enabled = true;
+                }
+            }
+            else
+            {
+                chkbBuscarNombre.Enabled = true;
+                if (txtBuscarCliente.Enabled == true)
+                {
+                    txtBuscarCliente.Enabled = false;
+                }
+            }
+        }
+    }
+    }
+
